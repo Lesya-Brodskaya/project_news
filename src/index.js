8bc6cc61a53d86aa325d4b27a4e0eb65381c12df
George Artemov <oscurasgeorge@gmail.com>
2023-03-22 10:22:12 +0000

    weather-markup-start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './js/calendar';
+import './js/weather-widget';
---
+++ src/js/weather-widget.js
@@ -0,0 +1,29 @@
+const API_KEY = 'c6d27dc8c63eae4b1bf25b80583f432d';
+const URL = 'https://api.openweathermap.org/data/2.5/weather?';
+
+function getWeatherInfo(city) {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+  )
+    .then(resolve => {
+      return resolve.json();
+    })
+    .then(weather => {
+      const celsiusTemp =
+        Math.round(weather.main.feels_like - 273.15) + '&deg;';
+
+      const markup = `<div class="weather-card"><div class="weather-card__info"><p class="weather-card__temperature">${celsiusTemp}</p><p class="weather-card__main">${weather.weather[0].main}</p><p class="weather-card__geolocation">${weather.name}</p><img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png"></div></div>`;
+      return document.body.insertAdjacentHTML('beforeend', markup);
+    });
+}
+
+// navigator.geolocation.getCurrentPosition(position => {
+//   getWeatherInfo(position.coords.latitude, position.coords.longitude);
+// });
+function getWeatherByGeolocation() {
+  fetch('https://ipinfo.io/json?token=2f70f8945bdfe0')
+    .then(response => response.json())
+    .then(currentCity => {
+      getWeatherInfo(currentCity.city);
+    });
+}
