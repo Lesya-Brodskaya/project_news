0cd063ec556c6d00ae17a1c1fb1d6f6b78585fe6
ivan1810 <78267912+ivan1810@users.noreply.github.com>
2023-03-23 14:09:12 +0200

    add theme-switcher

--- src/js/header.js
+++ src/js/header.js
@@ -8,11 +8,13 @@ const refs = {
   openMenuBtn: document.querySelector('.menu-open-btn'),
   themeSwitcher: document.querySelector('.theme-switcher'),
   formContainer: document.querySelector('.form-container'),
+  themeToggle: document.querySelector('#theme-toggle'),
 };
 refs.buttonShowInput.addEventListener('click', buttonShowInputClick);
 refs.form.addEventListener('submit', onFormSubmit);
 refs.buttonClose.addEventListener('click', onCloseMenu);
 refs.openMenuBtn.addEventListener('click', onMenuOpen);
+refs.themeToggle.addEventListener('click', toggleTheme);
 
 function onMenuOpen(event) {
   refs.navigation.classList.remove('hide');
@@ -36,4 +38,44 @@ function onFormSubmit(event) {
   event.preventDefault();
   const searchQuery = event.currentTarget.elements.search_input.value;
   console.log(searchQuery);
+}
+
+// for theme-switcher
+const LIGHT_THEME_CLASS = 'light-theme';
+const DARK_THEME_CLASS = 'dark-theme';
+const THEME_KEY = 'theme';
+let defaultTheme = LIGHT_THEME_CLASS;
+
+checkTheme();
+
+refs.themeToggle.addEventListener('click', toggleTheme);
+
+function toggleTheme() {
+  defaultTheme =
+    defaultTheme === LIGHT_THEME_CLASS ? DARK_THEME_CLASS : LIGHT_THEME_CLASS;
+  localStorage.setItem(THEME_KEY, defaultTheme);
+  applyTheme();
+}
+
+function applyTheme() {
+  const savedTheme = localStorage.getItem(THEME_KEY);
+
+  if (savedTheme === DARK_THEME_CLASS) {
+    document.body.classList.remove(LIGHT_THEME_CLASS);
+    document.body.classList.add(DARK_THEME_CLASS);
+  } else {
+    document.body.classList.remove(DARK_THEME_CLASS);
+    document.body.classList.add(LIGHT_THEME_CLASS);
+  }
+
+  refs.themeToggle.checked = savedTheme === DARK_THEME_CLASS;
+}
+
+function checkTheme() {
+  if (localStorage.getItem(THEME_KEY)) {
+    applyTheme();
+  } else {
+    localStorage.setItem(THEME_KEY, defaultTheme);
+  }
+  applyTheme();
 }
