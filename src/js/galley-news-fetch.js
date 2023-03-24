ceaea889cfd2c885da7a8641d6b9861dd79417d1
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-03-24 20:37:20 +0200

    no categories

--- src/js/galley-news-fetch.js
+++ src/js/galley-news-fetch.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=';
 const KEY = 'r45Ed0pk3C31WWBJaP0BSBDVSCCTnWUR';
 const BASE_POPULAR_URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?api-key=${KEY}`;
 
-const CATEGORIES = `https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${KEY}`;
+//const CATEGORIES = `https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${KEY}`;
 //https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=r45Ed0pk3C31WWBJaP0BSBDVSCCTnWUR
 //https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=r45Ed0pk3C31WWBJaP0BSBDVSCCTnWUR
 
@@ -47,7 +47,7 @@ export async function galleryFetchPopular(currentPage) {
     }
 
 }
-
+/*
 export async function galleryFetchCategories(currentPage) { 
     try { 
             //https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=yourkey
@@ -58,4 +58,4 @@ export async function galleryFetchCategories(currentPage) {
     catch (e) { 
         console.log(e.message);
     }
-}
+}*/
