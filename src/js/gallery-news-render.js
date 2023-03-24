ca259dd74f3969ea6d423f26990f25869738db29
George Artemov <oscurasgeorge@gmail.com>
2023-03-24 18:04:16 +0000

    Revert "Merge pull request #23 from Heorhii22/card-marcup"
    This reverts commit a52aab0e0935f216c8db602561ca1be910d577b1, reversing
    changes made to 6befdbdb45cb113b14a68bc312e9be235e4a0412.

--- src/js/gallery-news-render.js
+++ src/js/gallery-news-render.js
@@ -1,7 +1,6 @@
 import { galleryFetch } from './galley-news-fetch';
 import { galleryFetchPopular } from './galley-news-fetch';
-export { creatCardMarkup };
-  
+
 const searchForm = document.querySelector(".search-form");
 const newsGalleryLnk = document.querySelector(".news-gallery");
 
@@ -10,6 +9,18 @@ let currentHits = 0;
 let globalSearchQuery = '';
 
 searchForm.addEventListener('submit', onSearchBtn);
+onLoadNewsPage();
+
+async function onLoadNewsPage() { 
+  try { 
+    newsGalleryLnk.innerHTML = '';
+    const dataResponse = await galleryFetchPopular(currentPage);
+    console.log(dataResponse.data.results);
+  }
+  catch (e) { 
+    console.log(e.message);
+  }
+}
 
 function onSearchBtn(e) { 
     e.preventDefault();
@@ -40,9 +51,10 @@ async function operateDataBackEnd(searchQuery, searchPage) {
 }
 
 async function renderData(dataResponse) { 
-   console.log(dataResponse);  
+   //console.log(dataResponse);  
   const articles = dataResponse.docs;
-  await console.log(articles);
+   //console.log(articles);
+
   newsGalleryLnk.innerHTML = '';
   if (articles.length === 0) return;
   try { 
@@ -55,97 +67,31 @@ async function renderData(dataResponse) {
 }
 
 
-// async function readDataArrayToMarcup(articlesArray) {
-//   return await articlesArray.map(({ abstract, headline, keywords, multimedia, pub_date, snippet, web_url }) => {
-//     const firstImageUrl = multimedia.map((url) => { return url; })[0];
-//     console.log(firstImageUrl);
-//     const imageURL = "./src/images/mob/not-found-m.png";//multimedia[0];
-//     //if (multimedia.length !== 0) imagwURL = firstImageUrl;
-//     console.log(keywords);
-//     console.log(headline);
-//     const keywordsMap = keywords.map(({ value }) => { return value; }).join(', ');
-//     console.log(keywordsMap);
-//     return `
-//     <div class="news-gallery__item">
-//     <a class="news-gallery__image" href="${web_url}">
-//     <div class="news-gallery__img-container"><img src="${imageURL}"
-//     alt="${keywordsMap}" loading="lazy"/></div>
-//     </a>
-//     <div class="news-gallery__info">
-//       <p class="news-gallery__header">${headline.main}</p>
-//       <p class="news-gallery__abstract">${abstract}</p>
-//       <p class="news-gallery__pub_data">${pub_date}</p>
-//       <!--p class="news-gallery__snippet">${snippet}</p-->
-//     </div>
-//     <a class="news-gallery__read-more" href="${web_url}">Read more...</a>
-//     </div>
-//     `;
-//   }).join('');
-// }
+async function readDataArrayToMarcup(articlesArray) {
+  return await articlesArray.map(({ abstract, headline, keywords, multimedia, pub_date, snippet, web_url }) => { 
+    const firstImageUrl = multimedia.map((url) => { return url; });
+    //console.log("firstUrl", firstImageUrl[0].url);
 
-
-async function creatCardMarkup(el) {
-  return await el.map(({ abstract, title, url, published_date, image_url, section, id }) => {
-
-    const flags = checkRead(el);
-
-    return `<div class="container"><ul class="box-news"><li class="box-news__item ${
-    flags.read ? 'show' : ''
-  }" data-id="${id}"><p class="box-news__section">${section}</p>
-          <div class="box-news___wrap-reading">
-            <span class="box-news__reading">Alredy read</span>
-            <svg class="icon" width="18" height="18">
-              <use href="#icon-check"></use>
-            </svg> 
-          </div>
-          <button class="box-news__favorite-btn ${flags.fav ? 'favorite' : ''}">
-              <p class="box-news__favorite-p"> ${
-                flags.fav ? 'Remove from Favorite' : 'Add to Favorite'
-              }</p>
-              <svg class="box-news__favorite-svg" width="16" height="16"">
-                <use href="#icon-favorite"></use>
-              </svg>
-            </button>
-          <article>
-            <div class="box-news__thumb">
-              <img class="box-news__img" src="${image_url}" loading="lazy" alt="${section}" width='440'/>
-            </div>
-          <div class="box-news__wrap-title">
-            <h2 class="box-news__titel">
-              ${title}
-            </h2>
-          </div>
-          <div class="box-news__wrap-text">
-            <p class="box-news__text">
-              ${
-                abstract.length > 120
-                  ? abstract.slice(0, 110) + '...'
-                  : abstract
-              }
-            </p>
-          </div>
-          <div class="box-news__wrap">
-            <p class="box-news__data">${published_date}</p>
-            <a href="${url}" class="box-news__link" target="_blank" rel="noopener noreferrer nofollow">Read more</a>
-          </div>
-          </article>
-          <div class="overlay"></div></li></ul></div>`;
+    let imageURL = "https://www.nytimes.com/images/2015/03/14/arts/16iht-rartsquare-2/16iht-rartsquare-2-watch308.jpg"; //"./src/images/mob/not-found-m.png";
+      if (multimedia.length !== 0) imageURL = "https://www.nytimes.com/" + firstImageUrl[0].url;//articlesArray[0].multimedia[0].url;//firstImageUrl;
+    //console.log(web_url);
+    //console.log(imageURL);
+    const keywordsMap = keywords.map(({ value }) => { return value; }).join(', ');
+    //console.log(keywordsMap);
+    return `
+    <div class="news-gallery__item">
+    <a class="news-gallery__image" href="${web_url}">
+    <div class="news-gallery__img-container"><img src="${imageURL}" 
+    alt="${keywordsMap}" loading="lazy"/></div>
+    </a>
+    <div class="news-gallery__info">
+      <p class="news-gallery__header">${headline.main}</p>
+      <p class="news-gallery__abstract">${abstract}</p>
+      <p class="news-gallery__pub_data">${pub_date}</p>
+      <!--p class="news-gallery__snippet">${snippet}</p-->
+    </div>
+    <a class="news-gallery__read-more" href="${web_url}">Read more...</a>
+    </div>
+    `; 
   }).join('');
-};
-
-function checkRead(obj) {
-  let arrFav = loadAllFavorites() || [];
-
-  let objRead = loadReadStorage() || {};
-  let arrRead = Object.values(objRead).flat();
-
-  const answer = arrFav.find(item => obj.id === item.id);
-
-  const reply = arrRead.find(i => obj.id === i.id);
-  let result = [];
-
-  return (result = {
-    fav: answer ? true : false,
-    read: reply ? true : false,
-  });
 }
