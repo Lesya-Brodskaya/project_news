13248c03f2d4bb4360cce94c9341fa7980aafb41
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-03-24 23:01:54 +0200

    голова болить треба подумати

--- src/js/gallery-news-render.js
+++ src/js/gallery-news-render.js
@@ -4,6 +4,9 @@ import { galleryFetchPopular } from './galley-news-fetch';
 const searchForm = document.querySelector(".search-form");
 const newsGalleryLnk = document.querySelector(".news-gallery");
 
+let imageURL = "https://www.nytimes.com/images/2015/03/14/arts/16iht-rartsquare-2/16iht-rartsquare-2-watch308.jpg";
+const nytURL = "https://www.nytimes.com/";
+
 let currentPage = 1;
 let currentHits = 0;
 let globalSearchQuery = '';
@@ -52,14 +55,13 @@ async function renderData(articles) {
 
 
 async function readDataArrayToMarcup(articlesArray) {
-  let imageURL = "https://www.nytimes.com/images/2015/03/14/arts/16iht-rartsquare-2/16iht-rartsquare-2-watch308.jpg";
-  const nytURL = "https://www.nytimes.com/";
   //abstract, headline, keywords, multimedia, pub_date, snippet, web_url
   //      <!--p class="news-gallery__snippet">${snippet}</p-->
   return await articlesArray.map(({ abstract, headline, keywords, multimedia, pub_date, web_url }) =>
   { 
     const keywordsMap = keywords.map(({ value }) => { return value; }).join(', ');
-    const firstImageUrl = multimedia.map((url) => { return url; });
+
+    const firstImageUrl = multimedia.map((url) => { return url; });    
       if (firstImageUrl.length !== 0) imageURL = nytURL + firstImageUrl[0].url;
     
     return newsCardMarcup(web_url, imageURL, keywordsMap, headline.main, abstract, pub_date, web_url); 
@@ -95,6 +97,26 @@ async function onLoadNewsPage() {
     newsGalleryLnk.innerHTML = '';
     const dataResponse = await galleryFetchPopular(currentPage);
     console.log(dataResponse);
+    //abstract, headline, keywords, multimedia, pub_date, web_url
+    //headline.main
+    /*
+    abstract, adx_keywords, media, per_facet, 
+      media[0].url
+      per_facet.title,
+      per_facet.url
+    */
+    const popularArticlesArr = dataResponse.map(({ abstract, adx_keywords, media, per_facet }) => {
+      console.log();
+      console.log("abstract", abstract);
+      console.log("adx_keywords", adx_keywords);
+      console.log("media", media);
+      console.log("per_facet", per_facet);
+      console.log();
+      //console.log("media[0].url", media[0].url);
+      //console.log("per_facet.title", per_facet.title);
+      //console.log("per_facet.url", per_facet.url);
+      return;
+    });
   }
   catch (e) { 
     console.log(e.message);
