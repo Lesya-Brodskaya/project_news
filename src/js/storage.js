fbf5b1a9c81664892c724fc4a442f55cb1411002
ellinarel <113920230+ellinarel@users.noreply.github.com>
2023-03-25 00:33:32 +0200

    upd dates from storage

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,19 +1,66 @@
 const setLocalStorageItem = (key, value) => {
   try {
-    localStorage.setItem(key, JSON.stringify(value));
+    const dataToSave = {
+      date: new Date().toLocaleDateString(),
+      value: value,
+    };
+    localStorage.setItem(key, JSON.stringify(dataToSave));
   } catch (error) {
     console.error(`Error setting ${key} in local storage: ${error}`);
   }
 };
 
-const getLocalStorageItem = (key, defaultValue = null) => {
+const getLocalStorageItem = (key, date = null) => {
   try {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : defaultValue;
+    const data = localStorage.getItem(key);
+    if (!data) {
+      return;
+    }
+    const parsedData = JSON.parse(data);
+    if (!parsedData.hasOwnProperty('value')) {
+      return;
+    }
+    if (date !== null) {
+      const filteredData = parsedData.value.filter((item) => item.date === date);
+      return filteredData;
+    }
+    return parsedData.value;
   } catch (error) {
     console.error(`Error getting ${key} from local storage: ${error}`);
-    return defaultValue;
+    return null;
   }
 };
 
-export { setLocalStorageItem, getLocalStorageItem }
+function getReadingNewsFromStorage() {
+  return JSON.parse(localStorage.getItem('readingNews'));
+}
+
+function addReadingNewsToStorage(item) {
+  const addNews = item;
+  addNews.date = new Date().toLocaleDateString();
+  let readingNews = getReadingNewsFromStorage();
+
+  if (readingNews === null) {
+    readingNews = [addNews];
+  } else {
+    readingNews.push(addNews);
+  }
+  setLocalStorageItem('readingNews', readingNews);
+}
+
+function getReadingNewsDates() {
+  let readingNews = getReadingNewsFromStorage();
+  let arrDate = [];
+
+  for (let news of readingNews) {
+    if (!arrDate.includes(news.date)) {
+      arrDate.push(news.date);
+    }
+  }
+  return arrDate;
+}
+
+function getNewsByDate(date) {
+  let readingNews = getReadingNewsFromStorage();
+  return readingNews.filter((news) => news.date === date);
+}
