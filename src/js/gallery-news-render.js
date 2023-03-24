ed33dba0ec346441d537a24c0f130c50f5f8bbd4
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-03-24 21:31:20 +0200

    separate cad in my vision

--- src/js/gallery-news-render.js
+++ src/js/gallery-news-render.js
@@ -35,25 +35,20 @@ function onSearchBtn(e) {
     globalSearchQuery = search_input.value.trim();
     
     if (globalSearchQuery.length === 0) return;
-    //console.log(globalSearchQuery);
-
     operateDataBackEnd(globalSearchQuery, currentPage);
 }
 
 async function operateDataBackEnd(searchQuery, searchPage) { 
   try { 
     const data = await galleryFetch(searchQuery, searchPage);
-    await renderData(data);
+    await renderData(data.docs);
   }
   catch (e) { 
    console.log(e.message);
   }
 }
 
-async function renderData(dataResponse) { 
-   //console.log(dataResponse);  
-  const articles = dataResponse.docs;
-   //console.log(articles);
+async function renderData(articles) { 
 
   newsGalleryLnk.innerHTML = '';
   if (articles.length === 0) return;
@@ -68,30 +63,34 @@ async function renderData(dataResponse) {
 
 
 async function readDataArrayToMarcup(articlesArray) {
-  return await articlesArray.map(({ abstract, headline, keywords, multimedia, pub_date, snippet, web_url }) => { 
-    const firstImageUrl = multimedia.map((url) => { return url; });
-    //console.log("firstUrl", firstImageUrl[0].url);
-
-    let imageURL = "https://www.nytimes.com/images/2015/03/14/arts/16iht-rartsquare-2/16iht-rartsquare-2-watch308.jpg"; //"./src/images/mob/not-found-m.png";
-      if (multimedia.length !== 0) imageURL = "https://www.nytimes.com/" + firstImageUrl[0].url;//articlesArray[0].multimedia[0].url;//firstImageUrl;
-    //console.log(web_url);
-    //console.log(imageURL);
+  let imageURL = "https://www.nytimes.com/images/2015/03/14/arts/16iht-rartsquare-2/16iht-rartsquare-2-watch308.jpg";
+  const nytURL = "https://www.nytimes.com/";
+  //abstract, headline, keywords, multimedia, pub_date, snippet, web_url
+  //      <!--p class="news-gallery__snippet">${snippet}</p-->
+  return await articlesArray.map(({ abstract, headline, keywords, multimedia, pub_date, web_url }) =>
+  { 
     const keywordsMap = keywords.map(({ value }) => { return value; }).join(', ');
-    //console.log(keywordsMap);
-    return `
+    const firstImageUrl = multimedia.map((url) => { return url; });
+      if (firstImageUrl.length !== 0) imageURL = nytURL + firstImageUrl[0].url;
+    
+    return newsCardMarcup(web_url, imageURL, keywordsMap, headline.main, abstract, pub_date, web_url); 
+  }).join('');
+}
+
+//headline.main
+function newsCardMarcup(web_url, imageURL, keywordsMap, headline, abstract, pub_date, web_url) { 
+  return `
     <div class="news-gallery__item">
     <a class="news-gallery__image" href="${web_url}">
     <div class="news-gallery__img-container"><img src="${imageURL}" 
     alt="${keywordsMap}" loading="lazy"/></div>
     </a>
     <div class="news-gallery__info">
-      <p class="news-gallery__header">${headline.main}</p>
+      <p class="news-gallery__header">${headline}</p>
       <p class="news-gallery__abstract">${abstract}</p>
       <p class="news-gallery__pub_data">${pub_date}</p>
-      <!--p class="news-gallery__snippet">${snippet}</p-->
     </div>
     <a class="news-gallery__read-more" href="${web_url}">Read more...</a>
     </div>
-    `; 
-  }).join('');
+    `;
 }
