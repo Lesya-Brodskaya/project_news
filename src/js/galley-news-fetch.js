51e989f7b79f68d894be468cb11c4edacc343648
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-03-26 15:09:51 +0300

    corrected -

--- src/js/galley-news-fetch.js
+++ src/js/galley-news-fetch.js
@@ -1,46 +1,16 @@
 const axios = require('axios').default;
+//https://api.nytimes.com/svc/search/v2/articlesearch.json?q=panther&api-key=r45Ed0pk3C31WWBJaP0BSBDVSCCTnWUR
 
 const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=';
 const KEY = 'r45Ed0pk3C31WWBJaP0BSBDVSCCTnWUR';
-//https://api.nytimes.com/svc/search/v2/articlesearch.json?q=panther&api-key=r45Ed0pk3C31WWBJaP0BSBDVSCCTnWUR
-
 const BASE_POPULAR_URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${KEY}`;
+
+//const CATEGORIES = `https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${KEY}`;
 //https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=r45Ed0pk3C31WWBJaP0BSBDVSCCTnWUR
 //https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=r45Ed0pk3C31WWBJaP0BSBDVSCCTnWUR
 
-const CATEGORIES = `https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${KEY}`;
-
-
 let PER_PAGE = 40;
-// Доступ до бекенду за пошуком.
-export async function galleryFetch(queryLine, currentPage) {
-    let queryURL = `${BASE_URL}${queryLine}&api-key=${KEY}`;
-    return (accessFetch(queryURL)).response;
-}
 
-// Доступ до бекенду "популярних"
-export async function galleryFetchPopular(currentPage) {
-    let queryURL = BASE_POPULAR_URL;
-    return (accessFetch(queryURL)).results;
-}
-
-// Доступ до новин за категоріями
-export async function galleryFetchCategories(currentPage) { 
-    let queryURL = CATEGORIES;
-    return accessFetch(queryURL);
-}
-
-// Доступ до бекенду як такий
-async function accessFetch(queryURL) { 
-      try {
-    let response = await axios.get(queryURL);
-    return response.data;
-  } catch (e) {
-    console.log(e.message);
-  }
-}
-
-/*
 export async function galleryFetch(queryLine, currentPage) { 
 
     try {
@@ -65,4 +35,15 @@ export async function galleryFetchPopular(currentPage) {
     }
 
 }
-*/
+/*
+export async function galleryFetchCategories(currentPage) { 
+    try { 
+            //https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=yourkey
+        let response = await axios.get(CATEGORIES);
+        console.log("response", response);
+        return response;
+    }
+    catch (e) { 
+        console.log(e.message);
+    }
+}*/
