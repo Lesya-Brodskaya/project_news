663bed4ec2a423f04880f14f5a5d0f365c7663ee
George Artemov <oscurasgeorge@gmail.com>
2023-03-27 07:31:14 +0100

    weather-geo-fixed

--- src/js/weather/index.js
+++ src/js/weather/index.js
@@ -1,18 +1,8 @@
-// import { aaa } from '../popular_render';
-
-// console.log(aaa);
 import { format } from 'date-fns';
-const WEATHER_KEY = 'b61ccf4d4184c2f53b54bff2cbfc4f56';
+const WEATHER_KEY = 'c6d27dc8c63eae4b1bf25b80583f432d';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+let currentCity;
 
-// const refs = {
-//   degs: document.querySelector('.weather__deg'),
-//   weather: document.querySelector('.weather__value'),
-//   city: document.querySelector('.weather__city'),
-//   day: document.querySelector('.weather__day'),
-//   year: document.querySelector('.weather__year'),
-//   img: document.querySelector('.weather__img'),
-// };
 let refs = {};
 function getWeatherRefs() {
   //   console.log('refs');
@@ -29,8 +19,8 @@ function getWeatherRefs() {
   return refs;
 }
 
-async function fetchWeather() {
-  const url = `${BASE_URL}q=London&units=metric&appid=${WEATHER_KEY}`;
+async function fetchWeather(city) {
+  const url = `${BASE_URL}q=${city}&units=metric&appid=${WEATHER_KEY}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
@@ -41,18 +31,26 @@ async function fetchWeather() {
   }
 }
 
-async function fetchWeatherByGeo(lat, lon) {
-  const url = `${BASE_URL}lat=${lat}&lon=${lon}&appid=${WEATHER_KEY}&units=metric`;
+async function getUserGeolocation() {
+  const response = await fetch('https://ipinfo.io/json?token=2f70f8945bdfe0');
+  const userGeoInfo = await response.json();
+  currentCity = userGeoInfo.city;
+  return userGeoInfo.city;
+}
+
+getUserGeolocation();
+
+async function fetchWeatherByGeo(city) {
+  const url = `${BASE_URL}q=${city}&appid=${WEATHER_KEY}&units=metric`;
   try {
     const response = await fetch(url);
     const data = await response.json();
+    console.log(data);
     return data;
   } catch (error) {
     //  console.log(error);
   }
 }
-
-// console.log(fetchWeather());
 
 async function renderWeater() {
   const data = await fetchWeather();
@@ -72,28 +70,23 @@ async function renderWeater() {
 }
 
 async function getGeoposition() {
-  if (navigator.geolocation) {
-    await navigator.geolocation.getCurrentPosition(
-      ({ coords: { latitude, longitude } }) => {
-        fetchWeatherByGeo(latitude, longitude).then(data => {
-          const { temp } = data.main;
-          const weather = data.weather[0];
-          const { icon } = data.weather[0];
-          refs.degs.textContent = `${Math.floor(temp)}°`;
-          refs.weather.textContent = weather.main;
-          refs.city.textContent = data.name;
+  {
+    fetchWeatherByGeo(currentCity).then(data => {
+      const { temp } = data.main;
+      const weather = data.weather[0];
+      const { icon } = data.weather[0];
+      refs.degs.textContent = `${Math.floor(temp)}°`;
+      refs.weather.textContent = weather.main;
+      refs.city.textContent = data.name;
 
-          refs.day.textContent = format(new Date(), 'eee');
-          refs.year.textContent = format(new Date(), 'dd LLL y');
-          refs.img.setAttribute(
-            'src',
-            `https://openweathermap.org/img/wn/${icon}@4x.png`
-          );
-        });
-      }
-    );
+      refs.day.textContent = format(new Date(), 'eee');
+      refs.year.textContent = format(new Date(), 'dd LLL y');
+      refs.img.setAttribute(
+        'src',
+        `https://openweathermap.org/img/wn/${icon}@4x.png`
+      );
+    });
   }
-  return;
 }
 
 // getGeoposition();
